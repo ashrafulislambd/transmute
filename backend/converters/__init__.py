d281dbd149aaf92b700e5a1c480727965f74e2ed
Chase Roohms <chaseroohms@gmail.com>
2026-03-01 10:57:32 -0600

    feat(converters): add PyMuPDFConverter for PDF content extraction

--- backend/converters/__init__.py
+++ backend/converters/__init__.py
@@ -3,6 +3,7 @@ from .pillow_convert import PillowConverter
 from .pandas_convert import PandasConverter
 from .drawio_convert import DrawioConverter
 from .pypandoc_convert import PyPandocConverter
+from .pymupdf_convert import PyMuPDFConverter
 from .converter_interface import ConverterInterface
 
-__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "ConverterInterface"]
+__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "PyMuPDFConverter", "ConverterInterface"]
